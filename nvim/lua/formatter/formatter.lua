f64847d707cbfe693e54f96944e285278a989551
astaphobia <ariasta.ketut@icloud.com>
2024-07-06 10:14:56 +0800

    nvim - nvim/lua/all: format on save

--- nvim/lua/formatter/formatter.lua
+++ nvim/lua/formatter/formatter.lua
@@ -2,18 +2,23 @@ require("mason-null-ls").setup({
     ensure_installed = {
         -- Opt to list sources here, when available in mason.
 	"stylelua",
-	"prettierd",
-	"eslint",
-	"pint" -- laravel
+	"prettierd", -- prettier daemon for prettier
+	"prettier", -- formatter
+	"eslint_d",
+	"pint", -- laravel
+	"editorconfig-checker",
+	"shfmt",
+
     },
-    automatic_installation = false,
-    handlers = {},
+    methods = {
+	diagnostics = true,
+	formatting = true,
+	code_actions = true,
+	completion = true,
+	hover = true,
+    },
+    automatic_installation = true,
+    handlers  = {}
 })
 
-local null_ls = require("null-ls")
-null_ls.setup({
-    sources = {
-        -- Anything not supported by mason.
-    }
-})
-
+require("formatter.autoformat")
