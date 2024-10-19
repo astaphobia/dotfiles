fdc6b95b826ad96fd4940dce53ec7f464b48c175
astaphobia <ariasta.ketut@gmail.com>
2024-10-19 09:20:53 +0800

    nvim - nvim/lua/color/scheme: set default color to onedark

--- nvim/lua/color/scheme.lua
+++ nvim/lua/color/scheme.lua
@@ -1,10 +1,22 @@
 require("themer").setup({
 	colorscheme = "onedark",
+	transparent = true,
 	styles = {
 		["function"] = { style = "italic" },
 		functionbuiltin = { style = "italic" },
 		variable = { style = "italic" },
 		variablebuiltin = { style = "italic" },
-		parameter = { style = "italic" }
+		parameter = { style = "italic" },
+		todo = { style = "italic" }
+	},
+	langs = {
+		html = true,
+		md = true,
+		lua = true
 	}
 })
+
+-- Require 'nvim-telescope/telescope.nvim' installed
+-- Usage is :Telescope themes
+require("telescope").load_extension("themes")
+
